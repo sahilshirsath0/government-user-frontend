f30e9b5c7da514021da32097fc714aa2513edaf6
Sahil Shirsath <sahilshirsath75.com>
2025-12-19 22:10:36 +0530

    Update next.config and lockfile to fix build

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  experimental: {
-    optimizePackageImports: ['lucide-react'],
-  },
-}
 
-module.exports = nextConfig
+  // 🚫 DO NOT define optimizePackageImports at all
+  experimental: {},
+};
+
+module.exports = nextConfig;
